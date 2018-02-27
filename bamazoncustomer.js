fbb5083345db0ee687a2a25c5668fb74daf2fbba
Kofoworola Olabisi <toyosio@gmail.com>
2018-02-27 02:48:59 -0600

    ok

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -10,16 +10,15 @@ var connection = mysql.createConnection({
 	database: 'bamazon'
 })
 
-connection.connect(function(err) {
+connection.connect(function (err) {
 	if (err)
-	console.log('Connected as id' + connection.threadId);
+		console.log('Connected as id' + connection.threadId);
 	startBuying();
 })
 
 function printStuff(res) {
 	var table = new Table({
-		head: ['Item ID', 'Product Name', 'Department', 'Cost', 'Stock']
-		, colWidths: [10, 45, 40, 8, 8]
+		head: ['Item ID', 'Product Name', 'Department', 'Cost', 'Stock'],
 	});
 	for (var i = 0; i < res.length; i++) {
 		table.push([res[i].itemID, res[i].product_name, res[i].department_name, res[i].item_cost, res[i].stock_quantity]);
@@ -27,31 +26,34 @@ function printStuff(res) {
 	console.log(table.toString());
 }
 
-var startBuying = function() {
-	connection.query('SELECT * FROM products', function(err, res) {
+var startBuying = function () {
+	connection.query('SELECT * FROM products', function (err, res) {
 		printStuff(res);
-		var choiceArray = [];
+		var choice = [];
 		for (var i = 0; i < res.length; i++) {
 			choiceArray.push(res[i].product_name);
 		}
 		inquirer.prompt([{
-			name: 'item',
-			type: 'input',
-			message: 'Which item would you like to purchase? (Enter the Item ID)'
-		},
-		{
-			name: 'quantity',
-			type: 'input',
-			message: 'How many would you like to purchase?'
-		}]).then(function(answer) {
+				name: 'item',
+				type: 'input',
+				message: 'Which item would you like to purchase? (Enter the Item ID)'
+			},
+			{
+				name: 'quantity',
+				type: 'input',
+				message: 'How many would you like to purchase?'
+			}
+		]).then(function (answer) {
 			console.log(answer);
 			var itemID = answer.item;
 			console.log(itemID);
-			var chosenItem = res[itemID-1];
+			var chosenItem = res[itemID - 1];
 			console.log(chosenItem);
 			var newQuantity = chosenItem.stock_quantity - answer.quantity;
 			if (newQuantity >= 0) {
-				connection.query('UPDATE products SET ? WHERE itemID = ?', [{ stock_quantity: newQuantity }, itemID]);
+				connection.query('UPDATE products SET ? WHERE itemID = ?', [{
+					stock_quantity: newQuantity
+				}, itemID]);
 				startBuying();
 			} else {
 				console.log('There are not enough in stock for you to purchase that many.');
